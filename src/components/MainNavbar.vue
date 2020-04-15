3beeafcbd3c3cea06a10ce02cb48e6f51b3d4065
Unknown <namv221296@gmail.com>
2020-04-15 17:11:50 +0700

    Update navbar, banner, linkings and various

--- src/components/MainNavbar.vue
+++ src/components/MainNavbar.vue
@@ -1,40 +1,64 @@
 <template>
-  <div>
-    <parallax>
-    <img src="@/assets/Korone chase Risu.jpg" alt="very cool bg">
-    
-    <md-toolbar 
-    id="toolbar"
-    md-elevation="0"
-    class="md-transparent md-absolute"
-    :color-on-scroll="colorOnScroll">
-    <router-link to="/">
-      <h3 class="md-title">Hololike</h3>
-    </router-link>
+  <div id="app">
+    <md-toolbar md-elevation="0">
+      <div class="md-toolbar-section-start">
+        <router-link to="/">
+          <h1 class="md-title font_title" style="">Hololike</h1>
+        </router-link>
+      </div>
+      <div class="md-toolbar-section-end">
+        <md-list-item href="https://www.facebook.com/Hololike">
+          <img alt="Hololike FB" src="../assets/facebook.png" />
+        </md-list-item>
+
+        <md-list-item href="https://www.youtube.com/channel/UCwq3aenbgJR1ZWLvBi2JvTA">
+          <img alt="Hololike YT" src="../assets/youtube.png" />
+        </md-list-item>
+      </div>
     </md-toolbar>
-  </parallax>
+
+    <div style="position: relative;">
+      <parallax :fixed="true">
+        <img src="@/assets/Korone chase Risu.jpg" height="720px"/>
+        
+      </parallax>
+      <h1 class="font_name">Tớ Mang Clip Hololive Về Dịch</h1>
+    </div>
   </div>
 </template>
 
 <script>
-import Parallax from 'vue-parallaxy'
+import Parallax from "vue-parallaxy";
 
 export default {
-  name: 'RegularToolbar',
+  name: "RegularToolbar",
   components: {
-      Parallax
-    },
+    Parallax,
+  },
   props: {
     colorOnScroll: {
       type: Number,
-      default: 0
-    }
-  }
+      default: 0,
+    },
+  },
+  methods: {
+  },
 };
 </script>
 
 <style lang="scss" scoped>
-  .md-toolbar + .md-toolbar {
-    margin-top: 16px;
+.md-toolbar + .md-toolbar {
+  margin-top: 16px;
+}
+.font_title{
+  font-family: "Roboto";
+}
+.font_name{
+  font-size: 72px;
+  font-family: "000 Chinacat [TeddyBear]";
+  color: white;
+}
+.bottom-gradient {
+    background-image: linear-gradient(to top, rgba(0, 0, 0, 0.4) 0%, transparent 72px);
   }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,66 +1,65 @@
 <template>
   <div class="home">
     <MainNavbar></MainNavbar>
-  	<br/><br/>
-    <img alt="Hololike logo" src="../assets/watermark small.png" class="rotating">
-    <MainView msg="Work in progress, please come back later!!!"/>
+    <br/><br/><br/><br/>
+    <div style="background-color: #fff; height: 100vh;">
+      <img
+        alt="Hololike logo"
+        src="../assets/watermark small.png"
+        class="rotating"
+      />
+      <MainView msg="Work in progress, please come back later!!!" />
+    </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import MainView from '@/components/MainView.vue'
+import MainView from "@/components/MainView.vue";
 import MainNavbar from "@/components/MainNavbar";
 
 export default {
-  name: 'Home',
+  name: "Home",
   components: {
     MainView,
-    MainNavbar
-  }
-}
+    MainNavbar,
+  },
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-@keyframes rotating
-    {
-    from
-        {
-        transform: rotate(0deg);
-        -o-transform: rotate(0deg);
-        -ms-transform: rotate(0deg);
-        -moz-transform: rotate(0deg);
-        -webkit-transform: rotate(0deg);
-        }
-    to
-        {
-        transform: rotate(360deg);
-        -o-transform: rotate(360deg);
-        -ms-transform: rotate(360deg);
-        -moz-transform: rotate(360deg);
-        -webkit-transform: rotate(360deg);
-        }
-    }
-@-webkit-keyframes rotating
-    {
-    from
-        {
-        transform: rotate(0deg);
-        -webkit-transform: rotate(0deg);
-        }
-    to
-        {
-        transform: rotate(360deg);
-        -webkit-transform: rotate(360deg);
-        }
-    }
-.rotating
-    {
-    -webkit-animation: rotating 5s linear infinite;
-    -moz-animation: rotating 5s linear infinite;
-    -ms-animation: rotating 5s linear infinite;
-    -o-animation: rotating 5s linear infinite;
-    animation: rotating 5s linear infinite;
-    }
+@keyframes rotating {
+  from {
+    transform: rotate(0deg);
+    -o-transform: rotate(0deg);
+    -ms-transform: rotate(0deg);
+    -moz-transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+    -o-transform: rotate(360deg);
+    -ms-transform: rotate(360deg);
+    -moz-transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+  }
+}
+@-webkit-keyframes rotating {
+  from {
+    transform: rotate(0deg);
+    -webkit-transform: rotate(0deg);
+  }
+  to {
+    transform: rotate(360deg);
+    -webkit-transform: rotate(360deg);
+  }
+}
+.rotating {
+  -webkit-animation: rotating 5s linear infinite;
+  -moz-animation: rotating 5s linear infinite;
+  -ms-animation: rotating 5s linear infinite;
+  -o-animation: rotating 5s linear infinite;
+  animation: rotating 5s linear infinite;
+}
 </style>
