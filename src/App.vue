3bf7965e0beaf825c8679bd70d992d4991ca9363
Unknown <namv221296@gmail.com>
2020-12-28 22:17:54 +0700

    Remove footer

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,6 @@
             <!-- Simply add the component (NB! Add your page link as prop data-href="String" and your preferred link title as link-text="String") -->
             <vue-facebook-page data-href="https://www.facebook.com/Hololike" link-text="Tớ Mang Clip Hololive Về Dịch" />
         </div>
-        <router-view name="footer" />
     </div>
 </div>
 </template>
